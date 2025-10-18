f35cee2105456ecfa78997b12c4054b0ada85383
noonyuu <noon3m.op.sh@gmail.com>
2025-10-19 06:46:50 +0900

    feat: minioのやりとりをexpressで

--- back/storage/src/app.ts
+++ back/storage/src/app.ts
@@ -1,2 +1,36 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+
+import collageRouters from "./app/root";
+
+require("dotenv").config();
+const app = express();
+const router = express.Router();
+// 環境変数の読み込み
+const HOST_URL = process.env.HOST_URL || "";
+
+const corsOptions = {
+  origin: [HOST_URL],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.use(logger("dev")); // 開発用にログの表示
+app.use(express.json()); // application/jsonを扱えるようにする
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(router);
+
+app.use("/upload", collageRouters);
+
 // 接続確認用
 console.log("Hello World!!!");
+app.get("/ping", (_, req) => {
+  req.json({ message: "connected" });
+});
+
+export default app;
